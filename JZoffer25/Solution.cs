134a95fa5744a486acee9ead45acf2e96e7865a4
YuanTongCHN <yuantongchn@outlook.com>
2021-01-02 09:53:08 +0800

    添加项目文件。

---
+++ JZoffer25/Solution.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpJZoffer.JZoffer25
+{
+    class Solution
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpJZoffer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
