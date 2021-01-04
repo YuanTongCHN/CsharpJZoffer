b9b7798e8213626d5c5f17a1e0acff3efd11a2db
YuanTongCHN <yuantongchn@outlook.com>
2021-01-04 14:20:49 +0800

    v1

---
+++ JZOffer22/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpJZoffer.JZOffer22
+{
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public ListNode GetKthFromEnd(ListNode head, int k)
+        {
+
+            ListNode node1 = head;
+            ListNode node2 = head;
+            for (int i = 1; i <= k - 1; i++)
+            {
+
+                node1 = node1.next;
+            }
+            while (node1.next != null)
+            {
+
+                node1 = node1.next;
+
+                node2 = node2.next;
+            }
+            return node2;
+        }
+    }
+}
---
+++ JZOffer24/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpJZoffer.JZOffer24
+{
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode temp = null;
+            ListNode cur = head;
+            ListNode pre = null;
+            while (cur != null)
+            {
+                temp = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = temp;
+            }
+            return pre;
+
+
+        }
+    }
+}
