0a1134758dfe0e449972917fd5b6c14eccab120f
YuanTongCHN <yuantongchn@outlook.com>
2021-01-04 11:52:12 +0800

    v1

---
+++ JZOffer20/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpJZoffer.JZOffer20
+{
+
+    public class Solution
+    {
+        int index = 0;
+        public bool IsNumber(string s)
+        {
+            if (s.Length == 0) return false;
+            s = s + '|';
+            while (s[index] == ' ')
+            {
+                index++;
+            }
+            bool result = false;
+            result = ScanInteger(s);
+            if (s[index] == '.')
+            {
+                index++;
+                result = ScanUnsignInteger(s) || result;
+            }
+            if (s[index] == 'e' || s[index] == 'E')
+            {
+                index++;
+                result = ScanInteger(s) && result;
+            }
+            while (s[index] == ' ')
+            {
+                index++;
+            }
+            result = result && s[index] == '|';
+            return result;
+        }
+        public bool ScanInteger(string s)
+        {
+            if (s[index] == '+' || s[index] == '-')
+            {
+                index++;
+            }
+            return ScanUnsignInteger(s);
+        }
+        public bool ScanUnsignInteger(string s)
+        {
+            int flag = index;
+            while (s[index] >= '0' && s[index] <= '9')
+            {
+                index++;
+            }
+            return index > flag;
+        }
+    }
+
+}
---
+++ JZOffer21/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpJZoffer.JZOffer21
+{
+    public class Solution
+    {
+        public int[] Exchange(int[] nums)
+        {
+            int lflag = 0;
+            int rflag = nums.Length - 1;
+            while (lflag < rflag)
+            {
+                while (nums[lflag] % 2 != 0 && lflag < rflag)
+                {
+                    lflag++;
+                }
+                while (nums[rflag] % 2 == 0 && lflag < rflag)
+                {
+                    rflag--;
+                }
+                if (lflag < rflag)
+                {
+                    int temp = nums[lflag];
+                    nums[lflag] = nums[rflag];
+                    nums[rflag] = temp;
+                }
+            }
+            return nums;
+        }
+    }
+}
