a9289b82667322b4284465b33d9b8457c10eb091
YuanTongCHN <yuantongchn@outlook.com>
2021-01-07 10:35:11 +0800

    v1

---
+++ JZOffer42/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpJZoffer.JZOffer42
+{
+    public class Solution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            int sum = nums[0];
+            int maxSum = sum;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (sum < 0)
+                {
+                    sum = nums[i];
+                    if (maxSum < sum)
+                    {
+                        maxSum = sum;
+                    }
+                }
+                else
+                {
+                    sum += nums[i];
+                    if (maxSum < sum)
+                    {
+                        maxSum = sum;
+                    }
+                }
+            }
+            return maxSum;
+        }
+    }
+}
---
+++ JZOffer43/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpJZoffer.JZOffer43
+{
+    public class Solution
+    {
+        public int CountDigitOne(int n)
+        {
+            int result = 0;
+            int low = 0;
+            int digit = 1;
+            int high = n / 10;
+            int cur = n % 10;
+            while (high != 0 || cur != 0)
+            {
+                if (cur == 0)
+                {
+                    result += high * digit;
+                }
+                else if (cur == 1)
+                {
+                    result += high * digit + low + 1;
+                }
+                else
+                {
+                    result += (high + 1) * digit;
+                }
+                low = low + cur * digit;
+                cur = high % 10;
+                high = high / 10;
+                digit *= 10;
+            }
+            return result;
+        }
+    }
+}
