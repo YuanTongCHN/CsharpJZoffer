c55cefa7db61b14d71be677ff1feb084dad83214
YuanTongCHN <yuantongchn@outlook.com>
2021-01-11 15:48:43 +0800

    v1

---
+++ JZOffer52/Solution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpJZoffer.JZOffer52
+{
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null)
+            {
+                return null;
+            }
+            int countA = 1;
+            ListNode nodeA = headA;
+            ListNode nodeB = headB;
+            int countB = 1;
+            int d = 0;
+            while (true)
+            {
+                if (nodeA.next != null)
+                {
+                    countA++;
+                    nodeA = nodeA.next;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            while (true)
+            {
+                if (nodeB.next != null)
+                {
+                    countB++;
+                    nodeB = nodeB.next;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            nodeA = headA;
+            nodeB = headB;
+            if (countA > countB)
+            {
+                d = countA - countB;
+                for (int i = 0; i < d; i++)
+                {
+                    nodeA = nodeA.next;
+                }
+            }
+            if (countA < countB)
+            {
+                d = countB - countA;
+                for (int i = 0; i < d; i++)
+                {
+                    nodeB = nodeB.next;
+                }
+            }
+            while (nodeA != nodeB)
+            {
+                nodeA = nodeA.next;
+                nodeB = nodeB.next;
+            }
+            return nodeA;
+        }
+    }
+}
